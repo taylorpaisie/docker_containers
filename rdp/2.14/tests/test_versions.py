00dda27625bf7623ceb646e8a9705dfb58a3842a
taylorpaisie <tpaisie91@gmail.com>
2023-11-16 10:54:18 -0500

    started working on test controls

---
+++ rdp/2.14/tests/test_versions.py
@@ -0,0 +1,16 @@
+import unittest
+import subprocess
+
+
+class TestVersion(unittest.TestCase):
+    def test_pilon(self):
+        command = "classifier --version"
+        process = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        out, err = process.communicate()
+        self.assertEqual(out, b"RDP version 2.14\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
