d0502fbec5bdca4cb09f2171bf1847519258b372
taylorpaisie <tpaisie91@gmail.com>
2023-11-16 16:10:24 -0500

    modified test versions script

--- rdp/2.14/tests/test_versions.py
+++ rdp/2.14/tests/test_versions.py
@@ -4,12 +4,20 @@ import subprocess
 
 class TestVersion(unittest.TestCase):
     def test_rdp(self):
-        command = "classifier --version"
+        command = "classifier --version 2>&1 | awk '{print $4}'"
         process = subprocess.Popen(
             command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
         out, err = process.communicate()
-        self.assertEqual(out, "RDP version 2.14")
+        self.assertEqual(out, "v2.14\n")
+
+    def test_python(self):
+        command = "python --version 3>&1 | awk '{print $2}'"
+        process = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        out, err = process.communicate()
+        self.assertEqual(out, "3\n")
 
 
 if __name__ == "__main__":
