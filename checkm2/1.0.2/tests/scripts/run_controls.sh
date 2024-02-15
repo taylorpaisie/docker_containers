736284a114c479d11142671e4045f84f8dda17ca
taylorpaisie <tpaisie91@gmail.com>
2024-02-15 14:16:11 -0500

    made edits to checkm2 dockerfile and updated run_controls with test fastas

--- checkm2/1.0.2/tests/scripts/run_controls.sh
+++ checkm2/1.0.2/tests/scripts/run_controls.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Download test data
-wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/rdp/2.14/16S_rRNA_gene.Burkholderia_pseudomallei.2002721184.AY305776.1.fasta -O 16S_test.fa
-wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/rdp/2.14/18S_rRNA_gene.Homo_sapiens.T2T-CHM13v2.0.Chromosome13.fasta -O 18S_test.fa
+wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/TEST1.fasta -O TEST1.fasta
+wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/TEST2.fasta -O TEST2.fasta
+wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/TEST3.fasta -O TEST3.fasta
 
 # Get taxonomic assignments for your data
-classifier classify -o taxa_16S_test.txt 16S_test.fa
-classifier classify -o taxa_18S_test.txt 18S_test.fa
+checkm2 predict --threads 30 --input <folder_with_bins> --output-directory <output_folder> 
+
 
 # run checksum on files
-sha256sum 16S_test.fa > 16S_checksum.txt
-sha256sum 18S_test.fa > 18S_checksum.txt
+sha256sum TEST1.fasta > TEST1_checksum.txt
+sha256sum TEST2.fasta > TEST2_checksum.txt
+sha256sum TEST3.fasta > TEST3_checksum.txt
