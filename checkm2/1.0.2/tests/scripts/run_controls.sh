07ba6a0bb64b380caf5ed7af4cc93e1b62b20183
taylorpaisie <tpaisie91@gmail.com>
2024-02-15 14:42:44 -0500

    adding burk assembly from ncbi and updated run_controls script

--- checkm2/1.0.2/tests/scripts/run_controls.sh
+++ checkm2/1.0.2/tests/scripts/run_controls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download test data
-wget -nv https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_030010175.1/download?include_annotation_type=GENOME_FASTA -O burk_wgs_test.fa
+# wget -nv https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_030010175.1/download?include_annotation_type=GENOME_FASTA -O burk_wgs_test.fa
 
 # Get taxonomic assignments for your data
 checkm2 predict --input burk_wgs_test.fa --output-directory /data
