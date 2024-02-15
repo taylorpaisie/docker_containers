30d6b8c07de203ca7f691ff2b797020c96ca7421
taylorpaisie <tpaisie91@gmail.com>
2024-02-15 15:25:14 -0500

    updated checkm2 dockerfile and run_controls.sh

--- checkm2/1.0.2/tests/scripts/run_controls.sh
+++ checkm2/1.0.2/tests/scripts/run_controls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download test data
-wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/GCF_030010175.1_ASM3001017v1_genomic.fna.gz -O burk_wgs_test.fa.gz
+wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/GCF_030010175.1_ASM3001017v1_genomic.fna.gz -O burk_wgs_test.fna.gz
 wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/rdp/2.14/16S_rRNA_gene.Burkholderia_pseudomallei.2002721184.AY305776.1.fasta -O 16S_test.fa
 wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/neg_control_test.fna -O neg_control_test.fna
 
