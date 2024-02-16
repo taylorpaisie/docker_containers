779d0a148dd5653096b87daeaee723baca44ba21
taylorpaisie <tpaisie91@gmail.com>
2024-02-16 15:46:33 -0500

    changed checksum values to the correct ones for checkm2

--- checkm2/1.0.2/tests/test_controls.py
+++ checkm2/1.0.2/tests/test_controls.py
@@ -10,12 +10,20 @@ class TestControls(unittest.TestCase):
         subprocess.run(command, shell=True, stdout=PIPE)
 
 
-    def test_checkm2(self):
-        with open("checkm2_checksum.txt") as f:
+    def test_checkm2_wgs(self):
+        with open("burk_wgs_checksum.txt") as f:
             checkm2_checksum = f.readlines()[0].split(" ")[0]
         self.assertEqual(
             checkm2_checksum,
-            "a38342a9ba63946ffb4324c7858f5cc43b873673cb08080437f7500dda351f65",
+            "2811f35e88827c3f53562d231a0dd99e7c6542d942acad9ea2ab1f8203dc3d23",
+        )
+
+    def test_checkm2_16S(self):
+        with open("burk_16S_checksum.txt") as f:
+            checkm2_checksum = f.readlines()[0].split(" ")[0]
+        self.assertEqual(
+            checkm2_checksum,
+            "6620483f1d713575c914058f9ec1483ea5fe38414811fc46293ccc3659884d15",
         )
 
 
