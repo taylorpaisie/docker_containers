175beaa628f3b51fccd4bdf6640716fe45ea1c62
taylorpaisie <tpaisie91@gmail.com>
2024-02-16 10:34:08 -0500

    making edits to dockerfile and run_controls for pos/neg checkm2 testing

--- checkm2/1.0.2/tests/scripts/run_controls.sh
+++ checkm2/1.0.2/tests/scripts/run_controls.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Download test data
-wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/GCF_030010175.1_ASM3001017v1_genomic.fna.gz -O burk_wgs_test.fna.gz
-wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/rdp/2.14/16S_rRNA_gene.Burkholderia_pseudomallei.2002721184.AY305776.1.fasta -O 16S_test.fa
-wget -nv https://github.com/taylorpaisie/docker_containers/blob/main/checkm2/1.0.2/neg_control_test.fna -O neg_control_test.fna
+wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/checkm2/1.0.2/burk_wgs.fa -O burk_wgs_pos_ctrl.fa
+wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/checkm2/1.0.2/burk_16S.fa -O burk_16S_neg_ctrl.fa
+wget -nv https://raw.githubusercontent.com/taylorpaisie/docker_containers/main/checkm2/1.0.2/neg_control_test.fa -O neg_ctrl.fa
 
 # Get taxonomic assignments for your data
-checkm2 predict --input burk_wgs_test.fa \
-    16S_test.fa neg_control_test.fna \
-    --output-directory test_output/
+checkm2 predict --input burk_wgs_pos_ctrl.fa \
+    burk_16S_neg_ctrl.fa \
+    neg_ctrl.fa \
+    --output-directory tests_output/
 
 # run checksum on files
-sha256sum burk_wgs_test.fa > burk_wgs_test_checksum.txt
+sha256sum burk_wgs_pos_ctrl.fa > burk_wgs_checksum.txt
+sha256sum burk_16S_neg_ctrl.fa > burk_16S_checksum.txt
+sha256sum neg_ctrl.fa > neg_ctrl_checksum.txt
+
